27f4abf23bb94bf751e09bcd0ea3b966b28a78ae
Yohan <yohan@firebiscuit.com>
2024-01-12 16:04:06 +0800

    Enemy editor data classes parsing + validation

--- src/components/editor/EnemyEditor.jsx
+++ src/components/editor/EnemyEditor.jsx
@@ -1,57 +1,67 @@
 import { useEffect } from 'react';
+import {EnemyData} from '../../data/classes/EnemyData';
 
 export default function EnemyEditor({ moduleDescriptor }) {
   useEffect(() => {
     console.log(moduleDescriptor);
   }, [moduleDescriptor]);
 
+  if( ! moduleDescriptor ){
+    return <>Loading...</>;
+  }
+
+  const enemyData = EnemyData.fromJson(moduleDescriptor);
+  if( ! enemyData ){
+    return <>Invalid or corrupted data</>;
+  }
+  
   return (
     <>
       <div>
         <div>
-          <h5>GUID</h5>
-          <p>{moduleDescriptor?.guid}</p>
+          <h5>guid</h5>
+          <p>{enemyData.guid}</p>
           <h5>Name</h5>
-          <p>{moduleDescriptor?.name}</p>
+          <p>{enemyData.name}</p>
           <h5>Number of instances in deck</h5>
-          <p>{moduleDescriptor?.numInstancesInDeck}</p>
+          <p>{enemyData.numInstancesInDeck}</p>
           <h5>Enemy type</h5>
-          <p>{moduleDescriptor?.enemyType}</p>
+          <p>{enemyData.enemyType.name}</p>
           <h5>Armor</h5>
-          <p>{moduleDescriptor?.armor}</p>
+          <p>{enemyData.armor}</p>
           <h5>Is Elusive</h5>
-          <p>{moduleDescriptor?.isElusive.toString()}</p>
+          <p>{enemyData.isElusive ? 'Elusive' : 'Not Elusive'}</p>
           <h5>Elusive data</h5>
           <h6>Armor if blocked</h6>
-          <p>{moduleDescriptor?.elusiveData.armorIfBlocked}</p>
+          <p>{enemyData.elusiveData?.armorIfBlocked}</p>
           <h5>Fortification</h5>
-          <p>{moduleDescriptor?.fortification}</p>
+          <p>{enemyData.fortification.name}</p>
           <h5>XP gain</h5>
-          <p>{moduleDescriptor?.xpGain}</p>
+          <p>{enemyData.xpGain}</p>
           <h5>Reputation gain</h5>
-          <p>{moduleDescriptor?.reputationGain}</p>
+          <p>{enemyData.reputationGain}</p>
           <h5>Reputation gain bonus when rampaging</h5>
-          <p>{moduleDescriptor?.reputationGainBonusWhenRampaging}</p>
+          <p>{enemyData.reputationGainBonusWhenRampaging}</p>
           <h5>Challenge rating</h5>
-          <p>{moduleDescriptor?.challengeRating}</p>
+          <p>{enemyData.challengeRating}</p>
           <h5>Attacks</h5>
-          {moduleDescriptor?.attacks.map((attack, index) => (
-            <></>
+          {enemyData.attacks.map((atk, index) => (
+            <>{'['+atk.element.name+'] ('+atk.attackType.name+') '+atk.value+' '+atk.attackModifiers.map(mod => mod.name).join(',')}</>
           ))}
           <h5>Summoning attacks</h5>
-          {moduleDescriptor?.summoningAttacks.map((summoningAttack, index) => (
-            <></>
+          {enemyData.summoningAttacks.map((atk, index) => (
+            <>{atk.name}</>
           ))}
           <h5>Immunities</h5>
           <p>
-            {moduleDescriptor?.immunities.map((immunity, index) => (
-              <></>
+            {enemyData.immunities.map((immunity, index) => (
+               <>{immunity.name}</>
             ))}
           </p>
           <h5>Resistances</h5>
           <p>
-            {moduleDescriptor?.resistances.map((resistance, index) => (
-              <></>
+            {enemyData.resistances.map((resistance, index) => (
+              <>{resistance.name}</>
             ))}
           </p>
           <h5>Portrait sprite</h5>
---
+++ src/data/classes/EnemyData.js
@@ -0,0 +1,142 @@
+import {EnemyElusiveData} from './EnemyElusiveData';
+import {EnemyAttackData} from './EnemyAttackData';
+import {GameResources, FindResourceById} from '../GameResources';
+import {GameValues, FindEnumByValue} from '../GameValues';
+
+export class EnemyData {
+  constructor(){
+    // string. guid, unique only within the mod. Will be turned into guid 'mod:'+$yourModId+':'+$guid
+    this.guid = null;
+    // string. Enemy name (not localized)
+    this.name = null;
+    // int >= 0 [default=1]. If > 0, the enemy will be added to draw piles.
+    this.numInstancesInDeck = 1;
+    // GameResources.EnemyType
+    this.enemyType = null;
+    // int > 0 [default=1].
+    this.armor = 1;
+    // (Optional) bool [default=false]. If true, MUST specify elusive data.
+    this.isElusive = false;
+    // (Optional) ElusiveData. only specify if 'isElusive' is true.
+    this.elusiveData = null;
+    // (Optional) Fortification::int [default=NotFortified(2)]. Enum value
+    this.fortification = GameValues.Fortification.NotFortified;
+    // int >= 0. How much XP gained when defeating the enemy in battle.
+    this.xpGain = 0;
+    // (Optional) int >= 0 [default=0]. How much reputation gained when defeating the enemy in battle.
+    this.reputationGain = 0;
+    // (Optional) int >= 0 [default=1]. How much additional reputation gained when defeating the enemy in battle when it is rampaging.
+    this.reputationGainBonusWhenRampaging = 1;
+    // (Optional) int [1-10] [default=1]. How difficult is the enemy. Used for balancing when drawing randomly and for generating Battle Tale. Check the Wiki for example ratings.
+    this.challengeRating = 1;
+    // 	List<AttackData>. Can be empty if 'summoningAttacks' as at least one value.
+    this.attacks = [];
+    // List<EnemyType>. Type of enemy summoned by this attack. Can be empty if 'attacks' as at least one value.
+    this.summoningAttacks = [];
+    // List<Immunity>. List of immunity enum values.
+    this.immunities = [];
+    // List<Element>. List of element Ids the enemy is resistant to.
+    this.resistances = [];
+    // TODO(ylaunay) add pictures and tileData files
+  }
+
+  isValid(){
+    if( !this.guid ) return false;
+    if( !this.name ) return false;
+    if( this.numInstancesInDeck <= 0 ) return false;
+    if( !this.enemyType) return false;
+    if( this.armor <= 0 ) return false;
+    if( this.isElusive && (! this.elusiveData || ! this.elusiveData.isValid()) ) return false;
+    if( !this.fortification ) return false;
+    if( this.xpGain < 0 ) return false;
+    if( this.reputationGain < 0 ) return false;
+    if( this.reputationGainBonusWhenRampaging < 0 ) return false;
+    if( this.challengeRating < 1 || this.challengeRating > 10) return false;
+    if( this.attacks.length > 0 ){
+      for( let i = 0; i < this.attacks.length; i++ ){
+        const atk = this.attacks[i];
+        if( ! atk || ! atk.isValid() ){
+          return false;
+        }
+      }
+    }
+    if( this.summoningAttacks.length > 0 ){
+      for( let i = 0; i < this.summoningAttacks.length; i++ ){
+        const summonType = this.summoningAttacks[i];
+        if( ! summonType || ! FindResourceById(GameResources.EnemyType, summonType.value) ){
+          return false;
+        }
+      }
+    }
+    if( this.attacks.length === 0 && this.summoningAttacks.length === 0 ) return false;
+    if( this.immunities.length > 0 ){
+      for( let i = 0; i < this.immunities.length; i++ ){
+        const immunity = this.immunities[i];
+        if( ! immunity || ! FindEnumByValue(GameValues.Immunity, immunity.value) ){
+          return false;
+        }
+      }
+    }
+    if( this.resistances.length > 0 ){
+      for( let i = 0; i < this.resistances.length; i++ ){
+        const resistance = this.resistances[i];
+        if( ! resistance || ! FindEnumByValue(GameValues.Element, resistance.value) ){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // Throw if data is not valid
+  toJson(){
+    if( ! this.isValid() ) throw new Error('Invalid EnemyData');
+ 
+    const out = {};
+
+    out.guid = this.guid;
+    out.name = this.name;
+    out.numInstancesInDeck = this.numInstancesInDeck;
+    out.enemyType = this.enemyType.value;
+    out.armor = this.armor;
+    out.isElusive = this.isElusive;
+    if( this.isElusive ){
+      out.elusiveData = this.elusiveData.toJson();
+    }
+    out.fortification = this.fortification.value;
+    out.xpGain = this.xpGain;
+    out.reputationGain = this.reputationGain;
+    out.reputationGainBonusWhenRampaging = this.reputationGainBonusWhenRampaging;
+    out.challengeRating = this.challengeRating;
+    out.summoningAttacks = this.summoningAttacks.map(atk => atk.value);
+    out.immunities = this.immunities.map(immunity => immunity.value);
+    out.resistances = this.resistances.map(res => res.value);
+
+    return out;
+  }
+
+  static fromJson(json){
+    const data = new EnemyData();
+
+    data.guid = json.guid;
+    data.name = json.name;
+    data.numInstancesInDeck = json.numInstancesInDeck;
+    data.enemyType = FindResourceById(GameResources.EnemyType, json.enemyType);
+    data.armor = json.armor;
+    data.isElusive = !!json.isElusive && !!json.elusiveData;
+    if( data.isElusive ){
+      data.elusiveData = EnemyElusiveData.fromJson(json.elusiveData);
+    }
+    data.fortification = FindEnumByValue(GameValues.Fortification, json.fortification);
+    data.xpGain = json.xpGain;
+    data.reputationGain = json.reputationGain;
+    data.reputationGainBonusWhenRampaging = json.reputationGainBonusWhenRampaging;
+    data.challengeRating = json.challengeRating;
+    data.attacks = !!json.attacks ? json.attacks.map(atk => EnemyAttackData.fromJson(atk)) : [];
+    data.summoningAttacks = !!json.summoningAttacks ? json.summoningAttacks.map(atk => FindResourceById(GameResources.EnemyType, atk)) : [];
+    data.immunities = !!json.immunities ? json.immunities.map(immunity => FindEnumByValue(GameValues.Immunity, immunity)) : [];
+    data.resistances = !!json.resistances ? json.resistances.map(res => FindEnumByValue(GameValues.Element, res)) : [];
+
+    return data.isValid() ? data : null;
+  }
+}
