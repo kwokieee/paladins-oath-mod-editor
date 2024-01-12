479b55cacc9b18147b9f6dbdd990b97a1c6952bc
Bryan Kwok <bryankwokyf@gmail.com>
2024-01-12 16:17:37 +0800

    create simple interface for rewards editor

--- src/components/editor/RewardsEditor.jsx
+++ src/components/editor/RewardsEditor.jsx
@@ -1,12 +1,49 @@
 export default function RewardsEditor({ moduleDescriptor }) {
   return (
-    <>
-      <div>
-        <div>
-          <p>GUID</p>
-          <p>Name</p>
+    <div>
+      <h5>GUID</h5>
+      <input
+        type='text'
+        placeholder={'string. guid, unique only within the mod. Will be turned into GUID \'mod:\'+$yourModId+\':\'+$guid\''}
+        defaultValue={moduleDescriptor?.guid} 
+      />
+
+      <hr />
+
+      <h5>Name</h5>
+      <input
+        type='text'
+        placeholder={'string. Reward name (not localized) to be displayed when the rewards are distributed.'}
+        defaultValue={moduleDescriptor?.name}
+      />
+
+      <hr />
+
+      <h5>Combo type</h5>
+      <input
+        type='number'
+        placeholder={'(Optional) int (AND=0 | OR=1) [default=AND(0)]. Determines how the reward options are given to the player.'}
+        defaultValue={moduleDescriptor?.guid} 
+      />
+
+      <hr />
+
+      <h5>Reward options</h5>
+      <input
+        type='text'
+        placeholder={'string. Enemy name (not localized)'}
+        defaultValue={moduleDescriptor?.name}
+      />
+      {moduleDescriptor?.rewards.length === 0
+        ? <p>No rewards selected</p>
+        : moduleDescriptor?.rewards.map((reward, index) => (
+        <div key={index}>
+          <hr />
+          <p>{reward}</p>
         </div>
-      </div>
-    </>
+      ))}
+
+      <hr />
+    </div>
   );
 }
