0c8f94e7906702806d7c7ea505709ce3c6489ac2
Yohan <yohan@firebiscuit.com>
2024-01-12 17:47:02 +0800

    MapSection Data+Editor (readonly)

--- src/components/editor/MapSectionEditor.jsx
+++ src/components/editor/MapSectionEditor.jsx
@@ -1,10 +1,40 @@
+import { useEffect } from 'react';
+import {MapSectionData} from '../../data/classes/MapSectionData';
+
 export default function MapSectionEditor({ moduleDescriptor }) {
+    useEffect(() => {
+      console.log(moduleDescriptor);
+    }, [moduleDescriptor]);
+  
+    if( ! moduleDescriptor ){
+      return <>Loading...</>;
+    }
+  
+    const sectionData = MapSectionData.FromJson(moduleDescriptor);
+    if( ! sectionData ){
+      return <>Invalid or corrupted data</>;
+    }
+  
   return (
     <>
       <div>
         <div>
-          <p>GUID</p>
-          <p>Name</p>
+          <h5>guid</h5>
+          <p>{sectionData.guid}</p>
+          <h5>Num Instances In Deck</h5>
+          <p>{sectionData.numInstancesInDeck}</p>
+          <h5>Section Type</h5>
+          <p>{sectionData.sectionType.name}</p>
+          <h5>Hexes</h5>
+          {sectionData.hexes.map((hex, idx) => (
+            <>
+              <p>====</p>
+              <p>Terrain: {hex.terrain.id}</p>
+              <p>Location: {hex.location?.id ?? ''}</p>
+              <p>Enemy: {hex.rampagingEnemy?.id ?? ''}</p>
+              <p>EnemyType: {hex.rampagingEnemyType?.value ?? ''}</p>
+            </>
+          ))}
         </div>
       </div>
     </>
---
+++ src/data/classes/MapHexData.js
@@ -0,0 +1,57 @@
+import {GameValues, FindEnumByValue} from '../GameValues';
+import {GameResources, FindResourceById} from '../GameResources';
+
+export class MapHexData {
+  constructor(){
+    // Terrain::GUID.
+    this.terrain = null;
+    // (Optional) Location::GUID [default=NONE].
+    this.location = null;
+    // (Optional) EnemyType::GUID [default=NONE]. Enemy will be drawn from the list of enemies of this type. Either specify 'rampagingEnemyType' or 'rampagingEnemy' but not both.
+    this.rampagingEnemyType = null;
+    // (Optional) Enemy::GUID [default=NONE]. Enemy showing up on this hex. Either specify 'rampagingEnemyType' or 'rampagingEnemy' but not both.
+    this.rampagingEnemy = null;
+  }
+
+  isValid(){
+    if( !this.terrain ) return false;
+    if( !!this.location && ! FindResourceById(GameResources.Location, this.location.id)) return false;
+    if( !!this.rampagingEnemyType && !! this.rampagingEnemy ) return false;
+    if( !!this.rampagingEnemyType && ! FindEnumByValue(GameValues.EnemyType, this.rampagingEnemyType.value)) return false;
+    // TODO(ylaunay) support loading local mods GUIDs
+    if( !!this.rampagingEnemy && ! FindResourceById(GameResources.Enemy, this.rampagingEnemy.id)) return false;
+   
+    return true;
+  }
+
+  // Throw if data is not valid
+  toJson(){
+    if( ! this.isValid() ) throw new Error('Invalid MapHexData');
+ 
+    const out = {};
+
+    out.terrain = this.terrain.id;
+    if( !! this.location ){
+      out.location = this.location.value;
+    }
+    if( !! this.rampagingEnemy ){
+      out.rampagingEnemy = this.rampagingEnemy.id;
+    }else if( !! this.rampagingEnemyType ){
+      out.rampagingEnemyType = this.rampagingEnemyType.value;
+    }
+    
+    return out;
+  }
+
+  static FromJson(json){
+    const data = new MapHexData();
+
+    data.terrain = FindResourceById(GameResources.Terrain, json.terrain);
+    data.location = !! json.location ? FindResourceById(GameResources.Location, json.location) : null;
+    // TODO(ylaunay) support loading local GUIDs
+    data.rampagingEnemy = !! json.rampagingEnemy ? FindResourceById(GameResources.Enemy, json.rampagingEnemy) : null;
+    data.rampagingEnemyType = !! json.rampagingEnemyType ? FindEnumByValue(GameValues.EnemyType, json.rampagingEnemyType) : null;
+    
+    return data.isValid() ? data : null;
+  }
+}
