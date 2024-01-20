68f1d71abe00da35598a333530d6b6dbb05c9eb5
Bryan Kwok <bryankwokyf@gmail.com>
2024-01-21 01:19:49 +0800

    add value picker for rewards editor

--- src/components/editor/RewardsEditor.jsx
+++ src/components/editor/RewardsEditor.jsx
@@ -1,16 +1,13 @@
-import { useEffect } from 'react';
 import { RewardData } from '../../data/classes/RewardData';
+import ValuePicker from '../ValuePicker';
 
 export default function RewardsEditor({ moduleDescriptor }) {
-  useEffect(() => {
-    console.log(moduleDescriptor);
-  }, [moduleDescriptor]);
-
   if (!moduleDescriptor) {
     return <>Loading...</>;
   }
 
   const rewardData = RewardData.FromJson(moduleDescriptor);
+
   if (!rewardData) {
     return <>Invalid or corrupted data</>;
   }
@@ -40,31 +37,25 @@ export default function RewardsEditor({ moduleDescriptor }) {
       <hr />
 
       <h5>Combo type</h5>
-      <input
-        type="number"
-        placeholder={
-          '(Optional) int (AND=0 | OR=1) [default=AND(0)]. Determines how the reward options are given to the player.'
-        }
-        defaultValue={rewardData.guid}
-      />
+      <p>Determines how the reward options are given to the player.</p>
+      <ValuePicker valueType="RewardComboType" selected={rewardData.comboType} />
 
       <hr />
 
       <h5>Reward options</h5>
-      <input
-        type="text"
-        placeholder={'string. Enemy name (not localized)'}
-        defaultValue={rewardData.name}
-      />
+      {/* Multiselect */}
       {rewardData.rewardOptions.length === 0 ? (
         <p>No rewards selected</p>
       ) : (
-        rewardData.rewardOptions.map((reward, index) => (
-          <div key={index}>
-            <hr />
-            <p>{reward.name}</p>
-          </div>
-        ))
+        <div>
+          {rewardData.rewardOptions.map((reward, index) => (
+            <div key={index}>
+              <hr />
+              <p>{reward.name}</p>
+            </div>
+          ))}
+          <ValuePicker valueType="Reward" selected={rewardData.rewardOptions} />
+        </div>
       )}
 
       <hr />
