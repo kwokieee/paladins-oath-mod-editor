b4b11eb356a1a2f98584ad590b4b72d5fefad3f3
Bryan Kwok <bryankwokyf@gmail.com>
2024-01-30 13:15:59 +0800

    add module types

---
+++ src/data/ModuleTypes.js
@@ -0,0 +1,10 @@
+export const ModuleTypes = {
+  rewards: 'rewardsMods',
+  oath: 'oathMods',
+  character: 'characterMods',
+  enemy: 'enemyMods',
+  terrain: 'terrainMods',
+  mapSection: 'mapSectionMods',
+  scenario: 'scenarioMods',
+  scenarioExtension: 'scenarioExtensionMods',
+};
