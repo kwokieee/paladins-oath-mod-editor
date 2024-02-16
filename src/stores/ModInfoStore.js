f346d06747487bad580bdb71b1fab21c609b21da
Bryan Kwok <bryankwokyf@gmail.com>
2024-02-16 17:01:16 +0800

    store only relevant files in memory by creating new JSZip object

--- src/stores/ModInfoStore.js
+++ src/stores/ModInfoStore.js
@@ -42,12 +42,87 @@ export class ModInfoStore {
       return null;
     }
 
-    this.zip = zip;
+    const trimmedZip = await this.retrieveRelevantFiles(zip, modDescriptor, pathRoot);
+    console.log(trimmedZip);
+
+    this.zip = trimmedZip;
     this.pathRoot = pathRoot;
     this.modDescriptor = modDescriptor;
     await this.moduleStore.loadModulesUsing(this);
 
     this.hasMod = true;
+  }
+
+  async retrieveRelevantFiles(zip, modDescriptor, pathRoot) {
+    const trimmedZip = new JSZip();
+    await zip
+      .file(`${pathRoot}/mod_preview.jpg`)
+      .async('blob')
+      .then((content) => {
+        trimmedZip.file(`${pathRoot}/mod_preview.jpg`, content);
+      });
+    await zip
+      .file(`${pathRoot}/mod.json`)
+      .async('blob')
+      .then((content) => {
+        trimmedZip.file(`${pathRoot}/mod.json`, content);
+      });
+    const foldersOfInterest = [];
+    const filePatternsOfInterest = [];
+    modDescriptor.rewardsMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+    });
+    modDescriptor.oathMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+    });
+    modDescriptor.characterMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.png`));
+    });
+    modDescriptor.enemyMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.png`));
+    });
+    modDescriptor.terrainMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.png`));
+    });
+    modDescriptor.mapSectionMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filesOfInterest.push(`${pathRoot}/${moduleId}/mod.json`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+    });
+    modDescriptor.scenarioMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.png`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.lua`));
+    });
+    modDescriptor.scenarioExtensionMods.forEach((moduleId) => {
+      foldersOfInterest.push(`${pathRoot}/${moduleId}`);
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/mod.json`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.png`));
+      filePatternsOfInterest.push(new RegExp(`^${pathRoot}\/${moduleId}\/[^/]+\.lua`));
+    });
+    foldersOfInterest.forEach((folder) => {
+      trimmedZip.folder(folder);
+    });
+    await Promise.all(
+      zip
+        .filter((relativePath, file) => {
+          return filePatternsOfInterest.some((validPattern) => validPattern.test(relativePath));
+        })
+        .map(async (zipObject) => {
+          const content = await zipObject.async('blob');
+          trimmedZip.file(zipObject.name, content);
+        }),
+    );
+    return trimmedZip;
   }
 
   getModuleFolder(modId) {
@@ -68,13 +143,6 @@ export class ModInfoStore {
 
   export() {
     const modJson = this.modDescriptor.toJson();
-    // const exportedZip = new JSZip();
-    // exportedZip.file(`${this.pathRoot}/mod.json`, JSON.stringify(modJson, null, 2));
-    // this.moduleStore.export(exportedZip);
-    // exportedZip.generateAsync({ type: 'blob' }).then((content) => {
-    //   saveAs(content, 'mod.zip');
-    // });
-
     this.zip.file(`${this.pathRoot}/mod.json`, JSON.stringify(modJson, null, 2));
     this.moduleStore.export();
     this.zip.generateAsync({ type: 'blob' }).then((content) => {
